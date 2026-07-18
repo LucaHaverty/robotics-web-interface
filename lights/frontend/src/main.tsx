96423c82154874ec999a9b14eb82c5a20f596d58
LucaHaverty <lucahaverty@gmail.com>
2026-07-18 15:49:00 -0700

    Build frontend

--- lights/frontend/src/main.tsx
+++ lights/frontend/src/main.tsx
@@ -1,15 +1,9 @@
-import { useState, useRef } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { WebSocketProvider } from "./context/web-sockets-context";
 import App from "./App";
-import { isDev } from "./helpers";
 
 function Main() {
-  const [baseUrl] = useState(
-    isDev ? "http://localhost:5001" : "https://lights.lucahaverty.com",
-  );
-
   return (
     <WebSocketProvider>
       <App></App>
